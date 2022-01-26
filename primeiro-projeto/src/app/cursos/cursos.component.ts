d5c0625788afce19734891e43884301ee159fda0
jaque20050 <jaque20050@gmail.com>
2022-01-26 14:42:16 -0300

    Criando Templates

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.css']
+  styleUrls: ['./cursos.component.css'],
 })
 export class CursosComponent implements OnInit {
+  nomePortal: string;
 
-  constructor() { }
+  cursos: string[] = ['Java', 'Ext JS', 'Angular'];
 
-  ngOnInit(): void {
+  constructor() {
+    this.nomePortal = 'http://loiane.training';
+
+    for (let i = 0; i < this.cursos.length; i++) {
+      let curso = this.cursos[i];
+    }
   }
 
+  ngOnInit(): void {}
 }
