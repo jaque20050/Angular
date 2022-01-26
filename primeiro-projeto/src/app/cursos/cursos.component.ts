15173564db621cd20bd994b75b2238ebad022ea8
jaque20050 <jaque20050@gmail.com>
2022-01-26 15:00:18 -0300

    Criando o service

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
@@ -8,15 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class CursosComponent implements OnInit {
   nomePortal: string;
 
-  cursos: string[] = ['Java', 'Ext JS', 'Angular'];
+  cursos: string[];
 
-  constructor() {
+  constructor(public cursosService: CursosService) {
     this.nomePortal = 'http://loiane.training';
 
-    for (let i = 0; i < this.cursos.length; i++) {
+    /*for (let i = 0; i < this.cursos.length; i++) {
       let curso = this.cursos[i];
-    }
+    }*/
+
+    //var servico = new CursosService();
+
+    this.cursos = this.cursosService.getCursos();
   }
 
-  ngOnInit(): void {}
+  ngOnInit() {}
 }
