34e44ed1c842f6f0fc80b455b63245f30e33b359
jaque20050 <jaque20050@gmail.com>
2022-01-31 11:23:40 -0300

    Event binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -11,12 +11,33 @@ export class DataBindingComponent implements OnInit {
   cursoAngular: boolean = true;
   urlImagem ="https://picsum.photos/seed/picsum/400/200";
 
+  valorAtual: string ='';
+  valorSalvo = '';
+
+  isMouseOver: boolean = false;
+
   getValor(){
     return 1;
   }
 
   getCurtitCurso(){
     return true;
+  }
+
+  botaoClicado(){
+    alert('Botão clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor:any){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() { }
